4ac4e803d8068f9ac54627862330af5047945cb5
Carlos Gallo <carlos.gallo@somagrupo.com.br>
2021-07-20 19:43:58 -0300

    adds logic and style to done tasks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,34 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-  }
+    const newTask:Task = {
+      title:newTaskTitle,
+      id: (new Date().getTime()),
+      done:false
+    }
+    
+    setTasks([...tasks, newTask]);
+  } 
 
   function handleToggleTaskDone(id: number) {
+    
+    const updatedTasks:Task[]= tasks.map(task => ({ ...task }))
+    const taskToToggle = updatedTasks.find( task => task.id === id)
+    console.log(taskToToggle)
+    if(!taskToToggle){
+      return
+    }
+    taskToToggle.done=(!taskToToggle.done)
+    setTasks(updatedTasks)
+    //setMySkills(oldState => oldState.filter(skill => skill.id !== id));
+
     //TODO - toggle task done if exists
   }
 
+
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+
+    setTasks(tasks => tasks.filter((item) =>item.id !== id))
   }
 
   return (
@@ -33,7 +52,7 @@ export function Home() {
       />
     </View>
   )
-}
+  }
 
 const styles = StyleSheet.create({
   container: {
